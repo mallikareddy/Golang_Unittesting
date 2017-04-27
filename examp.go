95cf239722d26e76e90c80c3a1dbd90ea63d75d9
Mallika Reddy <mallika.manchala@gmail.com>
2017-04-27 20:39:44 +0530

    Update examp.go

--- examp.go
+++ examp.go
@@ -4,9 +4,9 @@ func main() {
    var num = []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 200}
    var sum int
    m := len(num)
-   for i := 0; i <= m; i++ {
+   for i := 0; i < m; i++ {
       fmt.Println("num[%d]:%d",i,num[i])
        sum = sum + num[i]
    }
-   fmt.Printf("sum is %d",sum)
+   fmt.Println("sum is %d",sum)
 }
