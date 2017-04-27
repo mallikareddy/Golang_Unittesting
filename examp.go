8c70fd1b8ba9856b2246a61c7fadd30d2ed2304f
Mallika Reddy <mallika.manchala@gmail.com>
2017-04-27 20:38:24 +0530

    Update examp.go

--- examp.go
+++ examp.go
@@ -1,12 +1,12 @@
 package main
 import "fmt"
 func main() {
-   var num = []int{10,20,30,40,50,60,70,80,90,200}
+   var num = []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 200}
    var sum int
    m := len(num)
    for i := 0; i <= m; i++ {
-       sum+=num[i]
-       i--
+      fmt.Println("num[%d]:%d",i,num[i])
+       sum = sum + num[i]
    }
    fmt.Printf("sum is %d",sum)
 }
