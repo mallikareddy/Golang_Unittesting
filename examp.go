8c1e7d085a22c59d06730513c6650c4445217097
Mallika Reddy <mallika.manchala@gmail.com>
2017-04-27 20:11:28 +0530

    Create examp.go

---
+++ examp.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+function main()
+{
+   var num = []int{10,20,30,40,50,60,70,80,90,200}
+   var sum int
+   var m int
+   m := len(num)
+   for i := 0; i <= m; i++
+   {
+       sum+=num[i]
+       i--
+   }
+   fmt.printf("sum is %d",sum)
+}
